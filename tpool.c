fd314a5f576046f62bb717aa856fe7cda8c4c54e
aatbip <anantabipal2010@gmail.com>
2026-03-13 23:27:35 +0545

    Handle failure during pthread_create

--- tpool.c
+++ tpool.c
@@ -1,10 +1,11 @@
 #include "tpool.h"
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _tpool {
   int thread_count;
-
 } tpool;
 
 void *worker(void *arg) {
@@ -16,10 +17,21 @@ void *worker(void *arg) {
 
 tpool *tpool_create(int thread_count) {
   tpool *tp = malloc(sizeof(*tp));
-  tp->thread_count = thread_count;
   pthread_t *th = malloc(sizeof(*th) * thread_count);
+  int th_failure = 0;
   for (int i = 0; i < thread_count; i++) {
-    pthread_create(th + i, NULL, worker, (void *)tp);
+    int s = pthread_create(th + i, NULL, worker, (void *)tp);
+    if (s != 0) {
+      th_failure++;
+      fprintf(stderr, "pthread_create: %s\n", strerror(s));
+    }
   }
+  if (th_failure >= thread_count) {
+    free(tp);
+    free(th);
+    return NULL;
+  }
+  /*`thread_count` should decrement if there failure during pthread_create.*/
+  tp->thread_count = thread_count - th_failure;
   return tp;
 }
