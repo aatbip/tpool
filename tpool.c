8f99fc48e85a8fc9b086fedc8b0ed177d8e53577
aatbip <anantabipal2010@gmail.com>
2026-03-13 23:10:50 +0545

    Basic function initialize tpool

---
+++ tpool.c
@@ -0,0 +1,24 @@
+#include "tpool.h"
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct _tpool {
+  int thread_count;
+
+} tpool;
+
+void *worker(void *arg) {
+  // worker thread which will execute jobs from the queue
+
+  return NULL;
+}
+
+tpool *tpool_create(int thread_count) {
+  tpool *tp = malloc(sizeof(*tp));
+  tp->thread_count = thread_count;
+  pthread_t *th = malloc(sizeof(*th) * thread_count);
+  for (int i = 0; i < thread_count; i++) {
+    pthread_create(th + i, NULL, worker, NULL);
+  }
+  return tp;
+}
