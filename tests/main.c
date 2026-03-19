452b8e09dc062c9178870c0742553d89dca685c6
aatbip <anantabipal2010@gmail.com>
2026-03-19 18:07:26 +0545

    Add job in the circular buffer using tpool_add

--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,22 @@
 /*Tests goes here.*/
 #include "tpool.h"
+#include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 
+void *job(void *arg) {
+  printf("job %d\n", (int)(uintptr_t)arg);
+  return NULL;
+}
+
 int main(void) {
-  tpool_create(10);
+  /*Create tpool with 4 worker threads.*/
+  tpool *tp = tpool_create(4);
+  assert(tp != NULL);
+
+  for (int i = 0; i < 50; i++) {
+    tpool_add(tp, job, (void *)(uintptr_t)i);
+  }
+
   return 0;
 }
