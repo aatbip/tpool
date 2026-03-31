794ff8fa1f2131d761ea051f270a99837db3a99a
aatbip <anantabipal2010@gmail.com>
2026-03-31 13:42:03 +0545

    Test concurrent increment

--- tests/ex2.c
+++ tests/ex2.c
@@ -1,15 +1,30 @@
 /*Test concurrent increment*/
+#include "tpool.h"
+#include <assert.h>
+#include <pthread.h>
 #include <stdio.h>
 
 int num = 0;
 
-void *increment(void *arg) {
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void increment(void *arg) {
+  pthread_mutex_lock(&mutex);
   num++;
-  return NULL;
+  pthread_mutex_unlock(&mutex);
 }
 
 int main(void) {
-  printf("running example 2");
+  tpool *tp = tpool_create(4, 50);
+  assert(tp != NULL);
+  for (int i = 0; i < 1000; i++) {
+    tpool_add(tp, increment, NULL);
+  }
+  tpool_wait(tp);
+  tpool_destroy(tp);
+
+  printf("num: %d\n", num);
+  assert(num == 1000);
 
   return 0;
 }
